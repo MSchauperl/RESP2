b3c89a90b5459ae365ebb90ae707964f36c480f2
MSchauperl <schauperl.michael@gmail.com>
2019-07-23 14:24:47 -0700

    Include Ethanol Example

--- resp2/charge_calculation.py
+++ resp2/charge_calculation.py
@@ -1,63 +1,9 @@
 import sys
 
 
-def combine2mol2(m1, m2):
-    f = open(m1, 'r')
-    f2 = open(m2, 'r')
-    v = 0
-    at = []
-    lines = f2.readlines()
-    for line in lines:
-        if '@<TRIPOS>ATOM' in line:
-            v = 1
-        elif '@<TRIPOS>BOND' in line:
-            v = 2
-        elif v == 1:
-            entry = line.split()
-            at.append(entry[5])
 
-
-    f2.close()
-    v = 0
-    num = 0
-    lines = f.readlines()
-    for line in lines:
-        if '@<TRIPOS>ATOM' in line:
-            v = 1
-            output.write(line)
-        elif '@<TRIPOS>BOND' in line:
-            v = 2
-            output.write(line)
-        elif v == 1:
-            entry = line.split()
-            output.write("{:>7} {:<3}{:>16}{:>10}{:>10} {:<4}{:>7}{:>5}{:>14}\n".format(entry[0], entry[1], entry[2], entry[3],
-                                                                            entry[4], at[num], entry[6], entry[7],
-                                                                          entry[8]))
-            num+=1
-        else:
-            output.write(line)
-    f.close()
-
-def charge_scaling(mol2_with_charges='', mol2_atomtypes=None):
-    f = open(m1, 'r')
-    if mol2_atomtypes != None:
-        f2 = open(mol2_atomtypes, 'r')
-    else:
-
-    v = 0
-    at = []
-    lines = f2.readlines()
-    for line in lines:
-        if '@<TRIPOS>ATOM' in line:
-            v = 1
-        elif '@<TRIPOS>BOND' in line:
-            v = 2
-        elif v == 1:
-            entry = line.split()
-            at.append(entry[5])
-     f2.close()
-
-
+def charge_scaling(mol2_with_charges=None, resname = 'MOL1'):
+    f = open(mol2_with_charges, 'r')
     v = 0
     num = 0
     lines = f.readlines()
@@ -74,24 +20,25 @@ def charge_scaling(mol2_with_charges='', mol2_atomtypes=None):
             entry = line.split()
             if float(entry[8])<0.0:
                 output.write("{:>7} {:<3}{:>15}{:>10}{:>10} {:<3}{:>8}{:>5}{:>14} # EVAL 8 0.000*(1.0-PRM['parameters.txt:1.0']){}*PRM['parameters.txt:1.0']\n".format(entry[0], entry[1], entry[2], entry[3],
-                                                                            entry[4], at[num], entry[6], entry[7],
+                                                                            entry[4], entry[5], entry[6], entry[7],
                                                                           entry[8],entry[8]))
             else:
                 output.write("{:>7} {:<3}{:>15}{:>10}{:>10} {:<3}{:>8}{:>5}{:>14} # EVAL 8 0.000*(1.0-PRM['parameters.txt:1.0'])+{}*PRM['parameters.txt:1.0']\n".format(entry[0], entry[1], entry[2], entry[3],
-                                                                            entry[4], at[num], entry[6], entry[7],
+                                                                            entry[4], entry[5], entry[6], entry[7],
                                                                           entry[8],entry[8]))
             num+=1
         else:
             output.write(line)
     f.close()
 
-def delta_resp2(m1, m2, m3):
-    f = open(m1, 'r')
-    f2 = open(m2, 'r')
-    f3 = open(m3, 'r')
+def delta_resp2(mol2_gas = None, mol2_liquid = None, resname = 'MOL1'):
+    f = open(mol2_gas, 'r')
+    f2 = open(mol2_liquid, 'r')
     v = 0
-    at = []
-    mol=[]
+
+    v = 0
+    # Read in implicit solvent charges (isc)
+    isc = []
     lines = f2.readlines()
     for line in lines:
         if '@<TRIPOS>ATOM' in line:
@@ -100,32 +47,15 @@ def delta_resp2(m1, m2, m3):
             v = 2
         elif v == 1:
             entry = line.split()
-            at.append(entry[5])
-            mol.append(entry[7])
-
-
-    f2.close()
-    v = 0
-    isc = []
-    lines = f3.readlines()
-    for line in lines:
-        if '@<TRIPOS>ATOM' in line:
-            v = 1
-        elif '@<TRIPOS>BOND' in line:
-            v = 2
-        elif v == 1:
-            entry = line.split()
             isc.append(float(entry[8]))
-
-
-    f3.close()
+    f2.close()
 
 
     v = 0
     num = 0
     lines = f.readlines()
     if lines[1].startswith('***') or lines[1].startswith('resp_gas') or lines[1].startswith('mol1_conf1'):
-        lines[1]='{}\n'.format(name)
+        lines[1]='{}\n'.format(resname)
     for i,line in enumerate(lines):
         if '@<TRIPOS>ATOM' in line:
             v = 1
@@ -137,11 +67,11 @@ def delta_resp2(m1, m2, m3):
             entry = line.split()
             if float(isc[num])<0.0:
                 output.write("{:>7} {:<3}{:>15}{:>10}{:>10} {:<3}{:>8}{:>5}{:>14} # EVAL 8 {}*(1.0-PRM['parameters.txt:1.0']){}*PRM['parameters.txt:1.0']\n".format(entry[0], entry[1], entry[2], entry[3],
-                                                                            entry[4], entry[5], entry[6], mol[num],
+                                                                            entry[4], entry[5], entry[6], resname,
                                                                           entry[8],entry[8],isc[num]))
             else:
                 output.write("{:>7} {:<3}{:>15}{:>10}{:>10} {:<3}{:>8}{:>5}{:>14} # EVAL 8 {}*(1.0-PRM['parameters.txt:1.0'])+{}*PRM['parameters.txt:1.0']\n".format(entry[0], entry[1], entry[2], entry[3],
-                                                                            entry[4], entry[5], entry[6], mol[num],
+                                                                            entry[4], entry[5], entry[6], resname,
                                                                           entry[8],entry[8],isc[num]))
             num+=1
         else:
@@ -174,38 +104,11 @@ def calc_parm(par_file,mol2_file):
 
     f.close()
 
-
-def calc_parm(par_file,mol2_file):
-    try:
-        para=float(par_file)
-    except:
-        f=open(par_file,'r')
-        para=float(f.readline().split()[0])
-        f.close()
-    print(para)
-    f=open(mol2_file,'r')
-    lines=f.readlines()
-    f.close()
-    f=open(mol2_file,'w')
-    for line in lines:
-        if "EVAL" in line:
-            entry=line.split()
-            c1=float(entry[12].split("*")[0])
-            c2=float(entry[12].split("*")[1].split(')')[1])
-            entry[8]=c1*(1.0-para)+c2*para
-            f.write('{0:>7} {1:<3} {2:>14} {3:>9} {4:>9} {5:<4} {6:>6} {7:>4} {8:>13.6f}\n'.format(*entry))
-        else:
-            f.write(line)
-
-    f.close()
-
-
-
 opt='normal'
 
 if __name__=='__main__':
     if len(sys.argv)<3:
-        print("Usage: python correct_atomypes_mol2.py charges.mol2 atomtyp.mol2 -out <output.mol2>--opt <normal/scale/resp2> <implicit_solvent_charges.mol2>")
+        print("Usage: python charge_calculation.py charges.mol2 atomtyp.mol2 -out <output.mol2>--opt <normal/scale/resp2> <implicit_solvent_charges.mol2>")
         exit(0)
     else:
         m1 = sys.argv[1]
--- resp2/resp2.py
+++ resp2/resp2.py
@@ -10,22 +10,22 @@ from openeye import oeomega
 import openforcefield
 import subprocess
 import logging as log
-from create_mol2_pdb import run_create_mol2_pdb
+import resp2.create_mol2_pdb
 import openbabel
 import shutil
 
+
 """
 Project functions
-create_data.csv
-create_target -- uses create data // create mol2 pdb.py
+XXXcreate_data.csv
+XXXcreate_target -- uses create data // create mol2 pdb.py
 create_fb_input
-DONE 1 create_conformers
-create_respyte_input (includes folder structure and all files)
-optimize_structures
-run_respyte
-make_resp1
-make_resp2 both using run_respyte
-functions from correct_atomtypes
+XXXXDONE 1 create_conformers
+XXXXcreate_respyte_input (includes folder structure and all files)
+XXXXXoptimize_structures
+XXXXXrun_respyte
+XXXXXXmake_resp1
+XXXXXXXmake_resp2 both using run_respyte
 """
 
 
@@ -68,7 +68,7 @@ def create_respyte(type='RESP1', name ='' ,resname ='MOL',number_of_conformers=1
         log.warning('folder {} already exists'.format(foldername))
 
     input_folder=os.path.join(foldername,'input')
-    molecule_folder=os.path.join(input_folder,'molecule')
+    molecule_folder=os.path.join(input_folder,'molecules')
     mol_folder=os.path.join(molecule_folder,'mol1')
     try:
         os.mkdir(input_folder)
@@ -86,7 +86,7 @@ def create_respyte(type='RESP1', name ='' ,resname ='MOL',number_of_conformers=1
     except:
         log.warning('folder {} already exists'.format(mol_folder))
 
-    for i in range(number_of_conformers):
+    for i in range(1,number_of_conformers+1):
         conf_folder=os.path.join(mol_folder,'conf'+str(i))
         try:
             os.mkdir(conf_folder)
@@ -97,37 +97,50 @@ def create_respyte(type='RESP1', name ='' ,resname ='MOL',number_of_conformers=1
     create_respyte_input_files(type=type, name =name ,resname =resname ,number_of_conformers=number_of_conformers)
 
 
-    """
-    Outsourced in optimization.yml
-    #2 Convert mol2 files to xyz files and put them in the corresponding folder
-    obConversion = openbabel.OBConversion()
-    obConversion.SetInAndOutFormats("mol2", "xyz")
-
-    foldername = os.path.dirname(name)
-    for i in range(number_of_conformers):
-        inputfile = os.path.join(foldername, resname + '-conformers_'+str(i)+'.mol2')
-        outputfile = os.path.join(foldername, resname + '-conformers_'+str(i)+'.xyz')
-
-        mol = openbabel.OBMol()
-        obConversion.ReadFile(mol, inputfile)
-        obConversion.WriteFile(mol,outputfile)
-    """
-
-
 
 
     #3 Copy optimized files
-    for i in range(number_of_conformers):
-        foldername = os.path.dirname(name)
-        shutil.copyfile(os.path.join(foldername,resname+'-confermers_opt_'+str(i)+'.xyz'), os.path.join('{}-{}/input/molecule/mol1/conf{}/mol1_conf{}.xyz'.format(name,type,i,i)))
+    for i in range(1,number_of_conformers+1):
+        foldername = os.path.join(os.path.dirname(name),name+'-liquid')
+        shutil.copyfile(os.path.join(foldername,resname+'-confermers_opt_'+str(i)+'.xyz'), os.path.join('{}-{}/input/molecules/mol1/conf{}/mol1_conf{}.xyz'.format(name,type,i,i)))
 
 
     #4 Generate input files
 
     create_respyte_input_files(type=type, name =name ,resname =resname,number_of_conformers=number_of_conformers)
 
+    cwdir = os.getcwd()
+    #5 Run RESPyt and PSI4
+    #os.chdir(foldername)
+    calculate_respyte(type=type, name =name ,resname =resname,number_of_conformers=number_of_conformers)
+    #os.chdir(cwdir)
     return 0
 
+
+def calculate_respyte(type='RESP1', name ='' ,resname ='MOL',number_of_conformers=1):
+    foldername = name + '-' + type
+    cwdr = os.getcwd()
+    os.chdir(foldername)
+    mol_folder = 'input/molecules/mol1/'
+    for i in range(1,number_of_conformers+1):
+        conf_folder=os.path.join(mol_folder,'conf'+str(i))
+        tmp_folder= os.path.join(conf_folder,'tmp/')
+        try:
+            shutil.rmtree(tmp_folder)
+        except:
+            pass
+    os.system('python ~/programs/respyte/respyte/esp_generator.py')
+    for i in range(1,number_of_conformers+1):
+        conf_folder=os.path.join(mol_folder,'conf'+str(i))
+        psi4_output_file = os.path.join(conf_folder,'tmp/output.dat')
+        if 'beer' in open(psi4_output_file).read():
+            log.info('ESP calculation for {} and conformer {} succesful'.format(name, i))
+        else:
+            log.error('ESP calculation for {} and conformer {} FAILED!!!!!!'.format(name, i))
+
+    os.system('python ~/programs/respyte/respyte/resp_optimizer.py')
+    os.chdir(cwdr)
+    return 0
 
 def create_respyte_input_files(type='RESP1', name ='' ,resname ='MOL',number_of_conformers=1):
 
@@ -150,30 +163,12 @@ def create_respyte_input_files(type='RESP1', name ='' ,resname ='MOL',number_of_
 
     #input.yml
     input_file = open('{}-{}/input/input.yml'.format(name,type),'w')
-    input_file.write("""# This file is a sample input.yml, input file for esp_generator.py
-# 'input.yml' should be included in 'input' directory with 'molecules' directory to run esp_generator.
-
-# In molecules, the number of molecules and the number of conformers for each molecule are assigned. 
-# For example, if the user wants to generate espf data for 2 different molecules with 5 conformers each, 
-# molecules is like below. (Each coordinate file should be either PDB or Mol2 file format and should be 
-# located in molecules/mol(i)/mol(j)/ ((i), (j) is integer) with its filename, mol(i)_mol(j).pdb(or .mol2)  
-# molecules : 
-#     mol1 : 5
-#     mol2 : 5
-molecules:
+    input_file.write("""molecules:
     mol1 : {}
-# In charges, the user should provide charge of each molecule. If not, it consider all molecules as neutral.
-# if mol1 is neutral and mol2 has -1 charge,  
-# charges :
-#     mol1 :  0
-#     mol2 : -1
 charges :
     mol1 : 0
-# In Cheminformatics, user can choose 'openeye' or 'rdkit' or 'None' 
-# But for now,  'None' option is not implemented yet, please consider use open-source 'rdkit' instead'
 cheminformatics : openeye
 
-# In grid_Setting, (explanation) 
 grid_setting :
     forcegen  : Y
     type      : msk # msk(default)/ extendedmsk/ fcc/ newfcc/ vdwfactors/ vdwconstants
@@ -194,67 +189,26 @@ grid_setting :
     #respyte.yml
     respyte_file = open('{}-{}/input/respyte.yml'.format(name,type),'w')
 
-    respyte_file.write("""## This file is a sample respyte.yml, input file for resp_optimzer.py
-    ## 'respyte.yml' should be included in 'input' directory', generated from running esp_generator.
-
-    ## Data structure (generated from running esp_generator.py or can manually be generated with an appropriate data structure):
-    ## input/
-    ## |----respyte.yml
-    ## |----molecules/
-    ##      |----mol1/
-    ##           |----conf1/
-    ##                |----mol1_conf1.pdb(or .mol2) , mol1_conf1.espf
-    ##           |----conf2/
-    ##                |----mol1_conf2.pdb(or .mol2) , mol1_conf2.espf
-    ## if xyz format has been used for esp generator, the data structure from esp_generator.py is containing pdb format converted from input xyz.
-
-    ## In molecules, the number of molecules and the number of conformers for each molecule are assigned.
-    ## For example, if the user wants to generate espf data for 2 different molecules with 5 conformers each,
-    ## molecules is like below. (Each coordinate file should be either PDB or Mol2 file format and should be
-    ## located in molecules/mol(i)/mol(j)/ ((i), (j) is integer) with its filename, mol(i)_mol(j).pdb(or .mol2)
-    ## molecules :
-    ##     mol1 : 5
-    ##     mol2 : 5
+    respyte_file.write("""
     molecules :
         mol1 : {}
-    ## In charges, user should specify a total charge of each residue. 
-    ## If the input molecule is small molecule, should specify net charge of each species.
     charges :
         mol1 : 0
 
-    ## In Cheminformatics, user can choose 'openeye' or 'rdkit' or 'None'
-    ## For now, rdkit option is not implemented yet.
     cheminformatics : openeye
 
-    ## In boundary_select, user can select subset of grid.dat
-    ## If boundary_select is not set, it uses all grid points in grid.dat for fitting.
     boundary_select:
         radii    : bondi
         inner    : 1.3
         outer    : 2.1
 
-    ## In restraint, user can specify which restraint(model2, model3 and two-stage fit) to use. 
     restraint :
-        # penalty : model2
-        # a       : 0.005
-
-        # penalty  : model3
-        # matrices :
-        #     - esp
-        # #    - ef
-        # a        : 0.0005
-        # b        : 0.1
-
         penalty : 2-stg-fit
         matrices :
             - esp
         a1      : 0.0005
         a2      : 0.001
         b       : 0.1
-
-    ## User can set user-defined constraints on specific charges 
-
-
 
         """.format(number_of_conformers))
     respyte_file.close()
@@ -292,7 +246,7 @@ def create_conformers(infile = None, outfile= None):
             oechem.OEWriteMolecule(ofs, mol)
             for k,conf in enumerate(mol.GetConfs()):
                 ofs2 = oechem.oemolostream()
-                if not ofs2.open(os.path.join(folder,filename+'_'+str(k)+'.mol2')):
+                if not ofs2.open(os.path.join(folder,filename+'_'+str(k+1)+'.mol2')):
                     oechem.OEThrow.Fatal("Unable to open %s for writing" % outfile)
                 oechem.OEWriteMolecule(ofs2, conf)
                 nconf = k+1
@@ -304,44 +258,21 @@ def create_conformers(infile = None, outfile= None):
     return nconf
 
 def create_std_target_file(name='',density=None,hov=None,dielectric=None):
-    header_csv = '''# This is documentation for the ForceBalance condensed phase reference data file,,,,,,
-    "# Lines beginning with octothorpe are comments, empty lines are ignored",,,,,,
-    "# A line can either be a comment, a global parameter, a single line with column headings, or a data line (after the column heading line)",,,,,,
-    # This file should be saved as .xlsx (to preserve formatting) but exported to .csv for ForceBalance to use.,,,,,,
-    ,,,,,,
-    "# Global parameters are defined here; they have ""Global"" in the first column.",,,,,,
-    # rho_denom : least squares denominator for the density objective function,,,,,,
-    "# Note: w_rho, w_hvap etc. is set in the input file.  This is because it's considered an adjustable option rather than a property of the data set.",,,,,,
-    # The overall prefactor for an observable is w_obs / obs_denom^2 ,,,,,,
-    ,,,,,,
-    Global,rho_denom,5,,,,
-    Global,hvap_denom,0.5,,,,
-    Global,alpha_denom,1,,,,
-    Global,kappa_denom,5,,,,
-    Global,cp_denom,2,,,,
-    Global,eps0_denom,2,,,,
-    Global,use_cvib_intra,FALSE,,,,
-    Global,use_cvib_inter,FALSE,,,,
-    Global,use_cni,FALSE,,,,
-    ,,,,,,
-    "# Before entering the data, there must be one line with column headings.",,,,,,
-    # Allowed column headings are: (not case sensitive),,,,,,
-    # T : Temperature in Kelvin,,,,,,
-    "# P : Pressure in specified unit (append atm or bar), default is atm",,,,,,
-    # MBAR : Include this state in multistate Bennett acceptance ratio calculation (more extreme states are not included),,,,,,
-    # Rho : Density in kg m^-3,,,,,,
-    # Hvap : Enthalpy of vaporization in kJ/mol.  Note: simulating liquid at 1 atm is an acceptable approximation for the vapor pressure.,,,,,,
-    # Alpha : Thermal expansion coefficient in 10^-4 K^-1.,,,,,,
-    # Kappa: Isothermal compressibility in 10^-6 bar^-1.,,,,,,
-    # Cp: Isobaric heat capacity in kJ/mol K^-1.,,,,,,
-    # Eps0: Static dielectric constant.,,,,,,
-    # Rho_wt : Weight to use for this phase point. Analogous for Hvap etc. If column is missing then all weights are 1.,,,,,,
-    # Cvib_intra : To be ADDED to the calculated enthalpy of vaporization ; energy difference due to intramolecular vibrational frequency shifts in going from gas to liquid,,,,,,
-    # Cvib_inter : To be ADDED to the calculated enthalpy of vaporization ; energy difference due to intermolecular vibrational frequency shifts in going from gas to liquid,,,,,,
-    # Cni : To be ADDED to the calculated enthalpy of vaporization ; accounts for the nonideality of the gas phase (more relevant close to critical point),,,,,,
-    # dEvib : To be ADDED to the calculated heat capacity.  Note that these values are slightly different from the TIP4P-Ew paper for some reason.,,,,,,
-    ,,,,,,
-    '''
+    header_csv = '''# This is documentation for the ForceBalance condensed phase reference data file
+,,,,,,
+,,,,,,
+Global,rho_denom,5,,,,
+Global,hvap_denom,0.5,,,,
+Global,alpha_denom,1,,,,
+Global,kappa_denom,5,,,,
+Global,cp_denom,2,,,,
+Global,eps0_denom,2,,,,
+Global,use_cvib_intra,FALSE,,,,
+Global,use_cvib_inter,FALSE,,,,
+Global,use_cni,FALSE,,,,
+,,,,,,
+,,,,,,
+'''
     if name == '':
         log.error('You did not specify a name for the target folder. Please use create_std_target_file(name=targetname,density=targetdensity,hov=target_heat,dielectric=target_eps0)')
     f=open(name+'-liquid/data.csv','w')
@@ -368,7 +299,7 @@ def create_target(smiles='',name='',density=None,hov=None,dielectric=None,resnam
     foldername = name+'-liquid/'
     create_std_target_file(name=name,density=density,hov=hov,dielectric=dielectric)
     create_smifile_from_string(smiles=smiles, filename=foldername+resname+'.smi',)
-    run_create_mol2_pdb(nmol = nmol, density = density - 100, tries = tries, input=foldername+resname+'.smi', resname=resname )
+    resp2.create_mol2_pdb.run_create_mol2_pdb(nmol = nmol, density = density - 100, tries = tries, input=foldername+resname+'.smi', resname=resname )
     #os.chdir(name+'-liquid')
     return 0
 
@@ -401,9 +332,9 @@ optimize('PW6B95')
     obConversion = openbabel.OBConversion()
     obConversion.SetInAndOutFormats("mol2", "xyz")
 
-    foldername = os.path.dirname(name)
+    foldername = os.path.join(os.path.dirname(name),name+'-liquid')
     filename = os.path.basename(name)
-    for i in range(number_of_conformers):
+    for i in range(1,number_of_conformers+1):
         inputfile = os.path.join(foldername, resname + '-conformers_'+str(i)+'.mol2')
         outputfile = os.path.join(foldername, resname + '-conformers_'+str(i)+'.xyz')
 
@@ -413,7 +344,7 @@ optimize('PW6B95')
 
     if opt == True:
 
-        for i in range(number_of_conformers):
+        for i in range(1,number_of_conformers+1):
             xyz_file = os.path.join(foldername, resname + '-conformers_'+str(i)+'.xyz')
             psi4_input_file =os.path.join(foldername, resname + '-conformers_'+str(i)+'.in')
             psi4_output_file =os.path.join(foldername, resname + '-conformers_'+str(i)+'.out')
@@ -433,17 +364,122 @@ optimize('PW6B95')
             os.system('psi4 {} -n 4'.format(psi4_input_file))
             if 'beer' in open(psi4_output_file).read():
                 log.info('Optimization of {} and conformer {} succesful'.format(filename,i))
+            else:
+                log.error('Optimization of {} and conformer {} FAILED!!!!!!'.format(filename,i))
 
     else:
-        for i in range(number_of_conformers):
-            shutil.copy(os.path.join(foldername,resname+'-confermers_'+str(i)+'.xyz'),os.path.join(foldername,resname+'-confermers_opt_'+str(i)+'.xyz'))
+        for i in range(1,number_of_conformers+1):
+            if not os.path.exists(os.path.join(foldername,resname+'-confermers_opt_'+str(i)+'.xyz')):
+                shutil.copy(os.path.join(foldername,resname+'-confermers_'+str(i)+'.xyz'),os.path.join(foldername,resname+'-confermers_opt_'+str(i)+'.xyz'))
 
+def create_charge_file(name = '', resname= 'MOL', delta = 0.0, type = 'RESP1'):
+    if type == 'RESP1':
+        mol2_resp1 = name+'-RESP1/resp_output/mol1_conf1.mol2'
+        f = open(mol2_resp1)
+        output_file= os.path.join(name+'-liquid',resname +'_R1_'+str(int(delta*100))+'.mol2')
+        output = open(output_file,'w')
+
+        # Read in RESP1 charges
+        v = 0
+        resp1charges = []
+        lines = f.readlines()
+        for line in lines:
+            if '@<TRIPOS>ATOM' in line:
+                v = 1
+            elif '@<TRIPOS>BOND' in line:
+                v = 2
+            elif v == 1:
+                entry = line.split()
+                resp1charges.append(float(entry[8]))
+        f.close()
+        charges = []
+        for i in range(len(resp1charges)):
+            charges.append(resp1charges[i]*delta)
+
+
+    elif type == 'RESP2':
+        mol2_gas = name+'-RESP2GAS/resp_output/mol1_conf1.mol2'
+        mol2_liquid = name+'-RESP2LIQUID/resp_output/mol1_conf1.mol2'
+        output_file= os.path.join(name+'-liquid',resname +'_R2_'+str(int(delta*100))+'.mol2')
+        f = open(mol2_gas, 'r')
+        f2 = open(mol2_liquid, 'r')
+        output = open(output_file,'w')
+
+        # Read in gas phase charges (gpc)
+        v = 0
+        gpc = []
+        lines = f.readlines()
+        for line in lines:
+            if '@<TRIPOS>ATOM' in line:
+                v = 1
+            elif '@<TRIPOS>BOND' in line:
+                v = 2
+            elif v == 1:
+                entry = line.split()
+                gpc.append(float(entry[8]))
+        f.close()
+
+        # Read in implicit solvent charges (isc)
+        v = 0
+        isc = []
+        lines2 = f2.readlines()
+        for line in lines2:
+            if '@<TRIPOS>ATOM' in line:
+                v = 1
+            elif '@<TRIPOS>BOND' in line:
+                v = 2
+            elif v == 1:
+                entry = line.split()
+                isc.append(float(entry[8]))
+        f2.close()
+
+        charges = []
+        for i in range(len(isc)):
+            charges.append(gpc[i]*(1.0-delta)+isc[i]*delta)
+        log.info('Created charges {} type charges with a delta value of {}'.format(type,delta))
+
+    else:
+        log.error('The type you defined is not recognized. Up to now only RESP1 and RESP2 are valid options')
+        sys.exit(1)
+
+
+    v = 0
+    num = 0
+    if lines[1].startswith('***') or lines[1].startswith('resp_gas') or lines[1].startswith('mol1_conf1'):
+        lines[1] = '{}\n'.format(resname)
+    for i, line in enumerate(lines):
+        if '@<TRIPOS>ATOM' in line:
+            v = 1
+            output.write(line)
+        elif '@<TRIPOS>BOND' in line:
+            v = 2
+            output.write(line)
+        elif v == 1:
+            entry = line.split()
+            output.write(
+            "{:>7} {:<3}{:>15}{:>10}{:>10} {:<3}{:>8}{:>5}{:>14.4f} \n".format(
+                        entry[0], entry[1], entry[2], entry[3],
+                        entry[4], entry[5], entry[6], resname,
+                        charges[num]))
+            num += 1
+        else:
+            output.write(line)
+    f.close()
+    output.close()
+
+
+    return 0
 
 if __name__ == "__main__":
     log.getLogger().setLevel(log.INFO)
-    create_target(name ='data/ethanol',density=789.3,hov = 42.3, dielectric=32.7,smiles='CCO',resname ='ETH')
+    #create_target(name ='data/ethanol',density=789.3,hov = 42.3, dielectric=32.7,smiles='CCO',resname ='ETH')
     number_of_conformers=create_conformers(infile='data/ETH.mol2', outfile='data/ETH-conformers.mol2')
-    optimize_conformers(name ='data/ethanol',resname ='ETH', opt = True,number_of_conformers=number_of_conformers)
-    create_respyte(name ='data/ethanol',resname ='ETH', type='RESP1',number_of_conformers=number_of_conformers)
+    #optimize_conformers(name ='data/ethanol',resname ='ETH', opt = False,number_of_conformers=number_of_conformers)
+    #create_respyte(name ='data/ethanol',resname ='ETH', type='RESP2LIQUID',number_of_conformers=number_of_conformers)
+    #create_respyte(name ='data/ethanol',resname ='ETH', type='RESP2GAS',number_of_conformers=number_of_conformers)
+    #create_respyte(name ='data/ethanol',resname ='ETH', type='RESP1',number_of_conformers=number_of_conformers)
+    #create_charge_file(name='data/ethanol',resname ='ETH', type = 'RESP1', delta= 2.0)
+
+
 
 
